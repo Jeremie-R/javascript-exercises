04e3c69dc3393eb5a362158b727e7cb061cb857c
farah <140409352+Jeremie-R@users.noreply.github.com>
2023-10-29 17:47:03 +0100

    i don't know yet

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+
+    let repeatedString = "";
+
+    for (let i = 0; i < times; i++) {
+        
+        repeatedString =+ string;
+        
+    }
+    return repeatedString;
 
 };
 
