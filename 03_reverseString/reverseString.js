ec3bd8f0e7f4dfa3e621174db82c2cfeb7f6881e
farah <140409352+Jeremie-R@users.noreply.github.com>
2023-11-01 17:32:58 +0100

    reverse string with array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let array = Array.from(string);
+    let reverseArray = array.reverse();
+
+    let reverseString = reverseArray.join("");
+
+    return  reverseString;
 
 };
 
