e5abc9c4d728b1a1949d51be365e3d53dc128b95
farah <140409352+Jeremie-R@users.noreply.github.com>
2023-11-01 17:47:06 +0100

    remove one form array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+    
+    let indexToRemove = array.indexOf(item);
+    if (indexToRemove > -1) { 
+        array.splice(indexToRemove, 1); 
+      }
+
+    return array;
 
 };
 
