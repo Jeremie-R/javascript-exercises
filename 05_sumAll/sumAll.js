be5a1b7397695d3aa985a03375265e3528517f4f
farah <140409352+Jeremie-R@users.noreply.github.com>
2023-11-01 18:19:53 +0100

    unexpected token

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+
+    if (Number.isInteger(first) = false) {
+        return "ERROR"
+    }
+    if (Number.isInteger(last) = false) {
+        return "ERROR"
+    }
+    if ((first < 0) ||  (last < 0)) {
+        return "ERROR"
+    }
+
+    //thank you math
+    if (first < last) {
+        let total = ((last-first+1)/2)*(first+last);
+        return total;
+
+    } else {
+        let total = ((firs-last+1)/2)*(first+last);
+        return total;
+    }
+
+
+    
 
 };
 
